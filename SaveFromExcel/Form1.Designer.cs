25e620c0acde19a86bc2e28474af43b54e43ed80
Axad <Axad@DESKTOP-CA1V8MR>
2020-07-30 18:22:32 +0500

    Add project files.

---
+++ SaveFromExcel/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace SaveFromExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1501, 647);
+            this.tabControl1.TabIndex = 0;
+            this.tabControl1.Tag = "";
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1493, 621);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "type_1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridView2);
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1493, 621);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "type_2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(81, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 27);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 33);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(1487, 585);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(0, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 29);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Open";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(81, 0);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 29);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Clear";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView2.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(0, 35);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.RowHeadersWidth = 35;
+            this.dataGridView2.Size = new System.Drawing.Size(1490, 583);
+            this.dataGridView2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1501, 646);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ SaveFromExcel/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.SqlServer.Server;
+using System.Windows.Forms;
+using Spire.Doc;
+using Spire.Doc.Documents;
+using Spire.Doc.Fields;
+using System.Runtime.InteropServices;
+using System.Threading;
+using OfficeOpenXml;
+using System.Globalization;
+using System.Drawing.Drawing2D;
+
+namespace SaveFromExcel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            initTables();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "XLS files (*.xls, *.xlt)|*.xls;*.xlt|XLSX files (*.xlsx, *.xlsm, *.xltx, *.xltm)|*.xlsx;*.xlsm;*.xltx;*.xltm|ODS files (*.ods, *.ots)|*.ods;*.ots|CSV files (*.csv, *.tsv)|*.csv;*.tsv|HTML files (*.html, *.htm)|*.html;*.htm";
+            openFileDialog.FilterIndex = 2;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                FileInfo file = new FileInfo(openFileDialog.FileName);
+                ExcelPackage excel = new ExcelPackage(file);
+                var worksheet = excel.Workbook.Worksheets[1];
+
+                dataGridView1.RowCount = worksheet.Dimension.Rows;
+                for (int k = 0; k < worksheet.Dimension.Rows - 1; k++)
+                {
+                    dataGridView1.Rows[k].Cells[1].Value = k + 1;
+                    dataGridView1.Rows[k].Cells[2].Value = worksheet.Cells[k + 2, 2].Value;
+                    
+                }
+            }
+        }
+
+        public void initTables()
+        {
+            dataGridView1.RowCount = 0;
+            dataGridView1.ColumnCount = 27;
+
+            dataGridView1.Columns[0].HeaderText = "#";
+            dataGridView1.Columns[1].HeaderText = "Стандарт";
+            dataGridView1.Columns[2].HeaderText = "Tok";
+            dataGridView1.Columns[3].HeaderText = "Серия";
+            dataGridView1.Columns[4].HeaderText = "Аппарат ";
+            dataGridView1.Columns[5].HeaderText = "Модель";
+            dataGridView1.Columns[6].HeaderText = "Отключ. Способность";
+            dataGridView1.Columns[7].HeaderText = "Pole ";
+            dataGridView1.Columns[8].HeaderText = "Кривая";
+            dataGridView1.Columns[9].HeaderText = "А";
+            dataGridView1.Columns[10].HeaderText = "mA";
+            dataGridView1.Columns[11].HeaderText = "Сторона";
+            dataGridView1.Columns[12].HeaderText = "REAR";
+            dataGridView1.Columns[13].HeaderText = "Вариант";
+            dataGridView1.Columns[14].HeaderText = "Iten code";
+            dataGridView1.Columns[15].HeaderText = "Full name";
+            dataGridView1.Columns[16].HeaderText = "Price";
+            dataGridView1.Columns[17].HeaderText = "SIZE";
+            dataGridView1.Columns[18].HeaderText = "Tочный вес";
+            dataGridView1.Columns[19].HeaderText = "BRUTTO";
+            dataGridView1.Columns[20].HeaderText = "NETTO";
+            dataGridView1.Columns[21].HeaderText = "Mbox CBM";
+            dataGridView1.Columns[22].HeaderText = "Mbox Qty";
+            dataGridView1.Columns[23].HeaderText = "MBox Weight";
+            dataGridView1.Columns[24].HeaderText = "QTY MBox/PBox";
+            dataGridView1.Columns[25].HeaderText = "PBox Weight";
+            dataGridView1.Columns[26].HeaderText = "Pbox CBM";
+
+            dataGridView1.Columns[0].Width = 50;
+            dataGridView1.Columns[15].Width = 200;
+
+            foreach (DataGridViewColumn col in dataGridView1.Columns)
+            {
+                col.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                col.SortMode = DataGridViewColumnSortMode.NotSortable;
+            }
+
+            dataGridView2.RowCount = 0;
+            dataGridView2.ColumnCount = 27;
+
+            dataGridView2.Columns[0].HeaderText = "#";
+            dataGridView2.Columns[1].HeaderText = "Стандарт";
+            dataGridView2.Columns[2].HeaderText = "Tok";
+            dataGridView2.Columns[3].HeaderText = "Серия";
+            dataGridView2.Columns[4].HeaderText = "Аппарат ";
+            dataGridView2.Columns[5].HeaderText = "Модель";
+            dataGridView2.Columns[6].HeaderText = "Размер";
+            dataGridView2.Columns[7].HeaderText = "Отключ. Способность";
+            dataGridView2.Columns[8].HeaderText = "Расцепитель";
+            dataGridView2.Columns[9].HeaderText = "А";
+            dataGridView2.Columns[10].HeaderText = "Pole";
+            dataGridView2.Columns[11].HeaderText = "Сторона";
+            dataGridView2.Columns[12].HeaderText = "REAR";
+            dataGridView2.Columns[13].HeaderText = "Вариант";
+            dataGridView2.Columns[14].HeaderText = "Iten code";
+            dataGridView2.Columns[15].HeaderText = "Full name";
+            dataGridView2.Columns[16].HeaderText = "Price";
+            dataGridView2.Columns[17].HeaderText = "SIZE";
+            dataGridView2.Columns[18].HeaderText = "Tочный вес";
+            dataGridView2.Columns[19].HeaderText = "BRUTTO";
+            dataGridView2.Columns[20].HeaderText = "NETTO";
+            dataGridView2.Columns[21].HeaderText = "Mbox CBM";
+            dataGridView2.Columns[22].HeaderText = "Mbox Qty";
+            dataGridView2.Columns[23].HeaderText = "MBox Weight";
+            dataGridView2.Columns[24].HeaderText = "QTY MBox/PBox";
+            dataGridView2.Columns[25].HeaderText = "PBox Weight";
+            dataGridView2.Columns[26].HeaderText = "Pbox CBM";
+
+            dataGridView2.Columns[0].Width = 50;
+            dataGridView2.Columns[15].Width = 200;
+
+            foreach (DataGridViewColumn col in dataGridView2.Columns)
+            {
+                col.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                col.SortMode = DataGridViewColumnSortMode.NotSortable;
+            }
+        }
+    }
+}
